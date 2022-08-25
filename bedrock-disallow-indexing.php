5ee61ccc749ac5d71e69cc5215f450cd367723da
Chauncey McAskill <chauncey@mcaskill.ca>
2022-08-25 14:52:04 -0400

    Add support for WP_ENVIRONMENT_TYPE
    New behaviour:
    
    1. Resolve the environment type:
    	1. Use the value of the `WP_ENV` constant if defined and palpable.
    	2. Otherwise, use the value of the `wp_get_environment_type()` function if defined (introduced in WordPress 5.0.0).
    	3. Otherwise, use `NULL`.
    2. Filter the environment type with the following hook:
    	* `roots/bedrock/disallow_indexing_environment_type`
    3. Display the notice:
    	1. If the environment type is palpable, display the notice with the filtered environment type.
    	2. Otherwise, display a shorter notice that does not reference the environment type.

--- bedrock-disallow-indexing.php
+++ bedrock-disallow-indexing.php
@@ -22,11 +22,28 @@ add_action('admin_init', function () {
     }
 
     add_action('admin_notices', function () {
-        $message = sprintf(
-            __('%1$s Search engine indexing has been discouraged because the current environment is %2$s.', 'roots'),
-            '<strong>Bedrock:</strong>',
-            '<code>'.WP_ENV.'</code>'
-        );
+        if (defined('WP_ENV') && WP_ENV) {
+            $wp_env = WP_ENV;
+        } else if (function_exists('wp_get_environment_type')) {
+            $wp_env = wp_get_environment_type();
+        } else {
+            $wp_env = null;
+        }
+
+        $wp_env = apply_filters('roots/bedrock/disallow_indexing_environment_type', $wp_env);
+        if ($wp_env) {
+            $message = sprintf(
+                __('%1$s Search engine indexing has been discouraged because the current environment is %2$s.', 'roots'),
+                '<strong>Bedrock:</strong>',
+                '<code>'.$wp_env.'</code>'
+            );
+        } else {
+            $message = sprintf(
+                __('%1$s Search engine indexing has been discouraged.', 'roots'),
+                '<strong>Bedrock:</strong>'
+            );
+        }
+
         echo "<div class='notice notice-warning'><p>{$message}</p></div>";
     });
 });
