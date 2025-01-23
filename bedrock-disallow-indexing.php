c31cfcc36a61584da31536312cbc376467ec82cb
Chauncey McAskill <chauncey@mcaskill.ca>
2025-01-23 13:04:26 -0500

    Apply suggestion from @retlehs
    Prefer fewer else statements and early returns.
    
    See: https://github.com/roots/bedrock-disallow-indexing/pull/1#issuecomment-2608867335

--- bedrock-disallow-indexing.php
+++ bedrock-disallow-indexing.php
@@ -22,28 +22,34 @@ add_action('admin_init', function () {
     }
 
     add_action('admin_notices', function () {
-        if (defined('WP_ENV') && WP_ENV) {
-            $wp_env = WP_ENV;
-        } else if (function_exists('wp_get_environment_type')) {
-            $wp_env = wp_get_environment_type();
-        } else {
-            $wp_env = null;
+        $env = defined('WP_ENV') && WP_ENV ? WP_ENV : null;
+
+        if (!$env && function_exists('wp_get_environment_type')) {
+            $env = wp_get_environment_type();
         }
 
-        $wp_env = apply_filters('roots/bedrock/disallow_indexing_environment_type', $wp_env);
-        if ($wp_env) {
-            $message = sprintf(
+        $env = apply_filters('roots/bedrock/disallow_indexing_environment_type', $env);
+
+        if (!$env) {
+            printf(
+                '<div class="notice notice-warning"><p>%s</p></div>',
+                sprintf(
+                    /* translators: %s: Bedrock prefix. */
+                    __('%s Search engine indexing has been discouraged.', 'roots'),
+                    '<strong>Bedrock:</strong>'
+                )
+            );
+            return;
+        }
+
+        printf(
+            '<div class="notice notice-warning"><p>%s</p></div>',
+            sprintf(
+                /* translators: 1: Bedrock prefix, 2: Environment type. */
                 __('%1$s Search engine indexing has been discouraged because the current environment is %2$s.', 'roots'),
                 '<strong>Bedrock:</strong>',
-                '<code>'.$wp_env.'</code>'
-            );
-        } else {
-            $message = sprintf(
-                __('%1$s Search engine indexing has been discouraged.', 'roots'),
-                '<strong>Bedrock:</strong>'
-            );
-        }
-
-        echo "<div class='notice notice-warning'><p>{$message}</p></div>";
+                '<code>' . $env . '</code>'
+            )
+        );
     });
 });
